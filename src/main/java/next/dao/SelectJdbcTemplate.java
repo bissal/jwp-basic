e33126852d9a5fdbf80164fb66c67925a7728a12
Gwak, Ok-Dong <gwak.okdong@gmail.com>
2016-07-18 16:29:44 +0900

    selectjdbc 까지.

--- src/main/java/next/dao/SelectJdbcTemplate.java
+++ src/main/java/next/dao/SelectJdbcTemplate.java
@@ -5,18 +5,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 
 import core.jdbc.ConnectionManager;
-import next.model.User;
 
 public abstract class SelectJdbcTemplate {
-	public List<User> executeQuery(String sql) {
+	public Object[] executeQuery(String sql) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		
-		ArrayList<User> userList = new ArrayList<User>();
+		ArrayList<Object> objList = new ArrayList<>();
 		try {
 			con = ConnectionManager.getConnection();
 			pstmt = con.prepareStatement(sql);
@@ -24,7 +22,10 @@ public abstract class SelectJdbcTemplate {
 
 			rs = pstmt.executeQuery();
 
-			mapRow(rs, userList);
+			while (rs.next()) {
+				Object obj = mapRow(rs);
+				objList.add(obj);
+			}
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		} finally {
@@ -43,22 +44,10 @@ public abstract class SelectJdbcTemplate {
 				throw new RuntimeException(e);
 			}
 		}
-		return userList;
+		return objList.toArray(new Object[objList.size()]);
 	}
-
-	private void mapRow(ResultSet rs, ArrayList<User> userList) throws SQLException {
-		User user = null;
-		
-		while (rs.next()) {
-			user = new User(
-					rs.getString("userId"), 
-					rs.getString("password"), 
-					rs.getString("name"),
-					rs.getString("email"));
-			userList.add(user);
-		}
-	}
-
+	
+	public abstract Object mapRow(ResultSet rs) throws SQLException;
 	public abstract void setValues(PreparedStatement pstmt)
 			throws SQLException;
 }
--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -1,13 +1,11 @@
 package next.dao;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import core.jdbc.ConnectionManager;
 import next.model.User;
 
 public class UserDao {
@@ -53,45 +51,59 @@ public class UserDao {
 			@Override
 			public void setValues(PreparedStatement pstmt) throws SQLException {
 				// TODO Auto-generated method stub
+			}
+
+			@Override
+			public Object mapRow(ResultSet rs) throws SQLException {
+				User user = new User(
+							rs.getString("userId"), 
+							rs.getString("password"), 
+							rs.getString("name"),
+							rs.getString("email"));
 				
+				return user;
 			}
 		};
 		
-		return selectJdbcTemplate.executeQuery(sql);
+		Object[] objs = selectJdbcTemplate.executeQuery(sql);
+		List<User> userList = new ArrayList<>();
+		for (Object object : objs) {
+			userList.add((User) object);
+		}
+		return userList;
 	}
 
 	public User findByUserId(String userId) throws SQLException {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			con = ConnectionManager.getConnection();
-			String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, userId);
+		String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
+		
+		SelectJdbcTemplate selectJdbcTemplate = new SelectJdbcTemplate() {
 
-			rs = pstmt.executeQuery();
-
-			User user = null;
-			if (rs.next()) {
-				user = new User(
+			@Override
+			public Object mapRow(ResultSet rs) throws SQLException {
+				User user = new User(
 						rs.getString("userId"), 
 						rs.getString("password"), 
 						rs.getString("name"),
 						rs.getString("email"));
+			
+				return user;
 			}
 
-			return user;
-		} finally {
-			if (rs != null) {
-				rs.close();
+			@Override
+			public void setValues(PreparedStatement pstmt) throws SQLException {
+				pstmt.setString(1, userId);
 			}
-			if (pstmt != null) {
-				pstmt.close();
-			}
-			if (con != null) {
-				con.close();
-			}
+		};
+		
+		Object[] objs = selectJdbcTemplate.executeQuery(sql);
+		if (objs.length != 1) {
+			return null;
 		}
+		
+		if (!(objs[0] instanceof User)) {
+			return null;
+		}
+		
+		return (User) objs[0];
 	}
 }
