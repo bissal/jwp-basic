30878be76a61a759134f6034ef3e38d95a0ab835
Gwak, Ok-Dong <gwak.okdong@gmail.com>
2016-07-18 17:49:31 +0900

    try-with-resources로 개선.

--- src/main/java/next/dao/JdbcTemplate.java
+++ src/main/java/next/dao/JdbcTemplate.java
@@ -10,67 +10,36 @@ import core.jdbc.ConnectionManager;
 
 public abstract class JdbcTemplate {
 	public void executeUpdate(String sql, PreparedStatementSetter psmtSetter) {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		try {
-			con = ConnectionManager.getConnection();
-			pstmt = con.prepareStatement(sql);
+		try (
+			Connection con = ConnectionManager.getConnection();
+			PreparedStatement pstmt = con.prepareStatement(sql);
+		){
 			psmtSetter.setValues(pstmt);
-
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-	
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException e) {
-				throw new RuntimeException(e);
-			}
 		}
 	}
 	
 	public Object[] executeQuery(String sql, PreparedStatementSetter psmtSetter, RowMapper mapper) {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 		
-		ArrayList<Object> objList = new ArrayList<>();
-		try {
-			con = ConnectionManager.getConnection();
-			pstmt = con.prepareStatement(sql);
+		try (
+			Connection con = ConnectionManager.getConnection();
+			PreparedStatement pstmt = con.prepareStatement(sql);
+		){
 			psmtSetter.setValues(pstmt);
-
-			rs = pstmt.executeQuery();
-
-			while (rs.next()) {
-				Object obj = mapper.mapRow(rs);
-				objList.add(obj);
+			
+			ArrayList<Object> objList = new ArrayList<>();
+			try (ResultSet rs = pstmt.executeQuery();) {
+				while (rs.next()) {
+					Object obj = mapper.mapRow(rs);
+					objList.add(obj);
+				}
 			}
+			return objList.toArray(new Object[objList.size()]);
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-	
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException e) {
-				throw new RuntimeException(e);
-			}
 		}
-		return objList.toArray(new Object[objList.size()]);
 	}
 	
 	public Object queryForObject(String sql, PreparedStatementSetter psmtSetter, RowMapper mapper) {
