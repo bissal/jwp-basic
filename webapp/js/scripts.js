9e8ee012318169b023e805ecb9de48fa8f01ddcc
Gwak, Ok-Dong <gwak.okdong@gmail.com>
2016-07-18 21:41:02 +0900

    ajax qna 등록까지.

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -1,3 +1,34 @@
+$(".answerWrite input[type=submit]").click(addAnswer);
+
+function addAnswer(e) {
+	e.preventDefault();
+	var queryString = $("form[name=answer]").serialize();
+	console.log('call addAnswer. ' + queryString);
+	
+	$.ajax({
+		type : 'post',
+		url : '/api/qna/addanswer',
+		data : queryString,
+		dataType : 'json',
+		error: onError,
+		success : onSuccess
+	});
+	
+	function onError(json, status){
+		console.log('onError');
+		console.log(json);
+	}
+	
+	function onSuccess(json, status){
+		console.log('onSuccess');
+		console.log(json);
+		
+		var answerTemplate = $("#answerTemplate").html();
+		var template = answerTemplate.format(json.writer, new Date(json.createdDate), json.contents, json.answerId, json.answerId);
+		$(".qna-comment-slipp-articles").prepend(template);
+	}
+}
+
 String.prototype.format = function() {
   var args = arguments;
   return this.replace(/{(\d+)}/g, function(match, number) {
